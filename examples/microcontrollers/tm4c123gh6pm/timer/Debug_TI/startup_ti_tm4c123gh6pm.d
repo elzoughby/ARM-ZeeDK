437b4a00bdbe659849ce904d5883a7da5d51dd5b
Ahmed Elzoughby <gs.elzoughby@gmail.com>
2017-10-02 21:01:07 +0200

    fix timer gnu build

--- examples/microcontrollers/tm4c123gh6pm/timer/Debug_TI/startup_ti_tm4c123gh6pm.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-startup_ti_tm4c123gh6pm.obj: ../startup_ti_tm4c123gh6pm.c
-startup_ti_tm4c123gh6pm.obj: ../config.h
-startup_ti_tm4c123gh6pm.obj: /home/ahmed/.ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h
-
-../startup_ti_tm4c123gh6pm.c: 
-../config.h: 
-/home/ahmed/.ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h: 
--- examples/microcontrollers/tm4c123gh6pm/timer/Debug_TI/timer2.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-timer2.obj: ../timer2.c
-timer2.obj: ../config.h
-
-../timer2.c: 
-../config.h: 
